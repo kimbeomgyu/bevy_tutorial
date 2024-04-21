65219e978bc71a9d79d3d3ea1d4c8e2e13d71fb4
kimbeomgyu <bg.kim@ejn.gg>
2024-04-21 14:57:38 +0900

    Ep1: ECS & Core Concepts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use bevy::prelude::*;
+
+#[derive(Component, Debug)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Component, Debug)]
+struct Velocity {
+    x: f32,
+    y: f32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_systems(Startup, spawn_spaceship)
+        .add_systems(Update, (update_position, print_position))
+        .add_plugins(DefaultPlugins)
+        .run();
+}
+
+fn spawn_spaceship(mut commands: Commands) {
+  commands.spawn((
+    Position { x: 0.0, y: 0.0 },
+    Velocity { x: 1.0, y: 1.0 },
+  ));
+}
+
+fn update_position(mut query: Query<(&mut Position, &Velocity)>) {
+  for (mut position, velocity) in query.iter_mut() {
+    position.x += velocity.x;
+    position.y += velocity.y;
+  }
+}
+
+fn print_position(query: Query<(Entity, &Position)>) {
+  // Log the entity ID and position of each entity with a `Position` component
+  for (entity, position) in query.iter() {
+    info!("Entity {:?} is at position: {:?}", entity, position);
+  }
 }
